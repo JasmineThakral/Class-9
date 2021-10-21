e414f1318469f4ddc35771906121e79e9cbe8c55
JasmineThakral <92955236+JasmineThakral@users.noreply.github.com>
2021-10-21 17:59:31 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+var box;//variable declaration
+function setup() {
+
+  createCanvas(400, 400);
+  box = createSprite(200,200,30,30);
+}
+
+function draw() {
+  background("pink");
+  drawSprites();
+}
